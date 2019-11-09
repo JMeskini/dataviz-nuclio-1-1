64f8cf56fd30cf7fe90b02355708a3fac058dc18
Francoeloy-dev <francoeloygonzalez@gmail.com>
2019-11-09 12:51:59 +0100

    Guillermo commit

--- src/charts/Sizes.js
+++ src/charts/Sizes.js
@@ -1,89 +1,70 @@
 import React, {useEffect, useState} from "react";
 import NVD3Chart from "react-nvd3";
 import {Typography} from "@material-ui/core";
-import makeStyles from "@material-ui/core/styles/makeStyles";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Grid from "@material-ui/core/Grid";
 import Switch from "@material-ui/core/Switch";
 
+const AntSwitch = withStyles(theme => ({
+  root: {
+    width: 28,
+    height: 16,
+    padding: 0,
+    display: 'flex',
+  },
+  switchBase: {
+    padding: 2,
+    color: theme.palette.grey[500],
+    '&$checked': {
+      transform: 'translateX(12px)',
+      color: theme.palette.common.white,
+      '& + $track': {
+        opacity: 1,
+        backgroundColor: theme.palette.primary.main,
+        borderColor: theme.palette.primary.main,
+      },
+    },
+  },
+  thumb: {
+    width: 12,
+    height: 12,
+    boxShadow: 'none',
+  },
+  track: {
+    border: `1px solid ${theme.palette.grey[500]}`,
+    borderRadius: 16 / 2,
+    opacity: 1,
+    backgroundColor: theme.palette.common.white,
+  },
+  checked: {},
+}))(Switch);
+
 export const SizesChart = ({data, gap = 100}) => {
   const d = {};
-  const [state, setState] = useState(true);
-
-  function DiameterMeters(){
-    data.filter(d => {
-      return d.estimated_diameter !== undefined;
-    }).map(d => d.estimated_diameter.meters.estimated_diameter_max)
-        .forEach(value => {
-          const category = Math.floor(value / gap);
-          if (!d[category]) {
-            d[category] =  0;
-          }
-          d[category] = d[category] + 1;
-        });
-    const datum = [];
-    Object.keys(d).forEach(key => {
-      datum.push({label: key * gap, value: d[key] });
-    });
-  }
-  function DiameterKilometers(){
-    data.filter(d => {
-      return d.estimated_diameter !== undefined;
-    }).map(d => d.estimated_diameter.kilometers.estimated_diameter_max)
-        .forEach(value => {
-          const category = Math.floor(value / gap);
-          if (!d[category]) {
-            d[category] =  0;
-          }
-          d[category] = d[category] + 1;
-        });
-  }
+  const [state, setState] = useState(false);
   const datum = [];
+  let typeDistance = 'meters';
+  let finalGap = gap;
+  if(state) {
+    finalGap = gap*10;
+    typeDistance = 'feet';
+  }
+
+
+  data.filter(d => {
+    return d.estimated_diameter !== undefined;
+  }).map(d => d.estimated_diameter[typeDistance].estimated_diameter_max)
+      .forEach(value => {
+        const category = Math.floor(value / finalGap);
+        if (!d[category]) {
+          d[category] =  0;
+        }
+        d[category] = d[category] + 1;
+      });
   Object.keys(d).forEach(key => {
     datum.push({label: key * gap, value: d[key] });
   });
 
-  const AntSwitch = withStyles(theme => ({
-    root: {
-      width: 28,
-      height: 16,
-      padding: 0,
-      display: 'flex',
-    },
-    switchBase: {
-      padding: 2,
-      color: theme.palette.grey[500],
-      '&$checked': {
-        transform: 'translateX(12px)',
-        color: theme.palette.common.white,
-        '& + $track': {
-          opacity: 1,
-          backgroundColor: theme.palette.primary.main,
-          borderColor: theme.palette.primary.main,
-        },
-      },
-    },
-    thumb: {
-      width: 12,
-      height: 12,
-      boxShadow: 'none',
-    },
-    track: {
-      border: `1px solid ${theme.palette.grey[500]}`,
-      borderRadius: 16 / 2,
-      opacity: 1,
-      backgroundColor: theme.palette.common.white,
-    },
-    checked: {},
-  }))(Switch);
-  useEffect(() => {
-    if( !state ) {
-      DiameterMeters();
-    }
-    else{
-      DiameterKilometers();
-    }
-  },[state]);
   return (<div>
     <div className="Grafico">
       <Typography>Sizes</Typography>
